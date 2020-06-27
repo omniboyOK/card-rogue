25188d8b227d687ccc5d5598c9b773d62d7a8b55
omniboyOk <kachorritou@gmail.com>
2020-06-27 01:54:40 -0300

    Moved to simple react-app

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
+import { Col, Row, Container } from 'react-bootstrap';
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <Container>
+        <Row>
+          <Col md={{span: 3, offset: 3}}>
+            <h2>Home</h2>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
