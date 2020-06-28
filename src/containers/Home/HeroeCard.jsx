d74d7913b6a57d31e7e21a56e91b02891ba7a904
omniboyOk <kachorritou@gmail.com>
2020-06-27 21:33:32 -0300

    changes to the structure of the app

---
+++ src/containers/Home/HeroeCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap'
+
+class HeroeCard extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Card>
+                
+            </Card>
+         );
+    }
+}
+ 
+export default HeroeCard;
