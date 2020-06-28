122b536bfbd9e4872c93c1ec6da28d44f1788f53
omniboyOk <kachorritou@gmail.com>
2020-06-28 01:21:11 -0300

    Home page as old home page with heroe cards

--- src/containers/Home/HeroeCard.jsx
+++ src/containers/Home/HeroeCard.jsx
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
-import { Card } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Card, Grid } from "semantic-ui-react";
 
 class HeroeCard extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <Card>
-                
-            </Card>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <Grid.Column>
+        <Card
+          color={this.props.heroe.color}
+          as="a"
+          image={process.env.PUBLIC_URL + this.props.heroe.portrait}
+          header={"Jugar como " + this.props.heroe.nombre}
+        />
+      </Grid.Column>
+    );
+  }
 }
- 
+
+HeroeCard.defaultProps = {
+  heroe: {
+    nombre: "Desconocido",
+    color: "grey",
+    ataque: 0,
+    defensa: 0,
+    maxVida: 0,
+    vida: 0,
+    maxMana: 0,
+    mana: 0,
+    portrait: "/heroes/placeholder.png",
+  },
+};
+
 export default HeroeCard;
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,12 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container, Segment, Input, Grid } from "semantic-ui-react";
+import HeroeCard from "./HeroeCard";
+
+var heroes = [
+  {
+    nombre: "Guerrero",
+    color: "red",
+    ataque: 60,
+    defensa: 50,
+    maxVida: 600,
+    vida: 600,
+    maxMana: 1000,
+    mana: 1000,
+    portrait: "/heroes/guerrero.png",
+  },
+  {
+    nombre: "Mago",
+    color: "blue",
+    ataque: 30,
+    defensa: 20,
+    maxVida: 400,
+    vida: 400,
+    maxMana: 2500,
+    mana: 2500,
+    portrait: "/heroes/mago.png",
+  },
+];
+
+let cards = heroes.map((heroe) => <HeroeCard heroe={heroe}></HeroeCard>);
 
 class Home extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div></div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <Container>
+        <Segment color="red" stacked className="mar">
+          <Grid columns="6">
+            <Grid.Row centered>
+              <Input
+                icon="pencil alternate"
+                placeholder="Escribe tu nombre aqui..."
+              ></Input>
+            </Grid.Row>
+
+            <Grid.Row>{cards}</Grid.Row>
+          </Grid>
+        </Segment>
+      </Container>
+    );
+  }
 }
- 
+
 export default Home;
