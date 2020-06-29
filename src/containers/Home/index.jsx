333556749270cd416aaeae455c1f40304c1eb234
omniboyOk <kachorritou@gmail.com>
2020-06-29 19:19:48 -0300

    Get heroes endpoint and dinamic creation of component

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -8,56 +8,40 @@ import {
 } from "semantic-ui-react";
 import HeroeCard from "./HeroeCard";
 import HeroeInfo from "./HeroeInfo";
-
-var heroes = [
-  {
-    nombre: "Guerrero",
-    color: "red",
-    ataque: 60,
-    defensa: 50,
-    maxVida: 600,
-    vida: 600,
-    maxMana: 1000,
-    mana: 1000,
-    portrait: "/heroes/guerrero.png",
-  },
-  {
-    nombre: "Mago",
-    color: "blue",
-    ataque: 30,
-    defensa: 20,
-    maxVida: 400,
-    vida: 400,
-    maxMana: 2500,
-    mana: 2500,
-    portrait: "/heroes/mago.png",
-  },
-];
 class Home extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            info: {},
-            visible: false
-        };
-        this.handleInfo = this.handleInfo.bind(this);
-    }
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: {},
+      visible: false,
+      heroes: [],
+    };
+    this.handleInfo = this.handleInfo.bind(this);
+  }
+
+  componentDidMount() {
+    this.getHeroes();
+  }
 
   handleInfo = (hero) => {
     this.setState({
-        info: hero,
-        visible: true
-    })
-  }
+      info: hero,
+      visible: true,
+    });
+  };
 
   getHeroes = () => {
-    fetch('/api/getHeroes')
-  }
+    fetch("http://localhost:5024/rpg/getHeroes")
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        this.setState({ heroes: myJson });
+      })
+      .catch((error) => console.log(error));
+  };
 
   render() {
-    let cards = heroes.map((heroe, i) => <HeroeCard key={i} heroe={heroe} action={this.handleInfo}></HeroeCard>);
-
     return (
       <Container>
         <SegmentGroup raised horizontal>
@@ -69,11 +53,22 @@ class Home extends Component {
                   placeholder="Escribe tu nombre aqui..."
                 ></Input>
               </Grid.Row>
-              <Grid.Row>{cards}</Grid.Row>
+              <Grid.Row>
+                {this.state.heroes.map((heroe, i) => (
+                  <HeroeCard
+                    key={i}
+                    heroe={heroe}
+                    action={this.handleInfo}
+                  ></HeroeCard>
+                ))}
+              </Grid.Row>
             </Grid>
           </Segment>
           <Segment color="grey">
-            <HeroeInfo heroe={this.state.info} visible={this.state.visible}></HeroeInfo>
+            <HeroeInfo
+              heroe={this.state.info}
+              visible={this.state.visible}
+            ></HeroeInfo>
           </Segment>
         </SegmentGroup>
       </Container>
