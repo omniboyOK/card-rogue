d509f224431e5cd301f35692d622e69278404603
omniboyOk <kachorritou@gmail.com>
2020-06-29 15:58:43 -0300

    Working server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const conn = require("./db/driverMongo");
 const routes = require("./routes");
+//const passport = require("passport");
 //const session = require("express-session");
 //require("./config/auth");
 const app = express();
@@ -12,16 +13,17 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb', extended: true}));
 app.use(cors());
 
-
 // en caso de error de conexion con la db
 conn.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use("/rpg/", routes);
+app.use("/api/", routes);
 
 app.use("/ping", (req, res) => {
   console.log("POST".red);
   res.status(200).send("pong");
 });
+
+//app.use(express.static("public"));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
